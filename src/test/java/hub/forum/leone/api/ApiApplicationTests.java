59801f4ae52bc02efce80648b3956e1eebb29da0
Rodrigo Leone <rodri.leone@gmail.com>
2024-07-13 18:40:22 -0300

    Primeiro commit

---
+++ src/test/java/hub/forum/leone/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.forum.leone.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
